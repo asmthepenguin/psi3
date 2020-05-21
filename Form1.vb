417301fa2f031c503f499950cf310004f78f0e63
alexandra madeira <63977887+asmthepenguin@users.noreply.github.com>
2020-05-21 21:37:39 +0100

    Add files via upload

---
+++ Form1.vb
@@ -0,0 +1,6 @@
+﻿Public Class Form1
+    Private Sub btnFazerLogin_Click(sender As Object, e As EventArgs) Handles btnFazerLogin.Click
+        Dim f As New Form2
+        f.Show()
+    End Sub
+End Class
